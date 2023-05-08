acb62c153e8a038e72bcce6842bc9f40ef557439
Kristo Kuusküll <kristo.kuuskull@transferwise.com>
2023-05-08 15:33:36 +0300

    Added support for Spring Boot 3.0.

--- tw-base-utils/src/main/java/com/transferwise/common/baseutils/concurrency/DefaultExecutorServicesProvider.java
+++ tw-base-utils/src/main/java/com/transferwise/common/baseutils/concurrency/DefaultExecutorServicesProvider.java
@@ -2,23 +2,31 @@ package com.transferwise.common.baseutils.concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.annotation.PostConstruct;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.InitializingBean;
 
-public class DefaultExecutorServicesProvider implements IExecutorServicesProvider {
+public class DefaultExecutorServicesProvider implements IExecutorServicesProvider, InitializingBean {
 
   private ExecutorService executorService;
   private ScheduledTaskExecutor scheduledTaskExecutor;
 
   private boolean initialized = false;
 
-  @PostConstruct
-  public synchronized void init() {
-    if (!initialized) {
-      executorService = Executors.newCachedThreadPool(new CountingThreadFactory("tw-base"));
-      scheduledTaskExecutor = new SimpleScheduledTaskExecutor(null, executorService);
-      scheduledTaskExecutor.start();
-      initialized = true;
+  private Lock initializationLock = new ReentrantLock();
+
+  public void afterPropertiesSet() {
+    initializationLock.lock();
+    try {
+      if (!initialized) {
+        executorService = Executors.newCachedThreadPool(new CountingThreadFactory("tw-base"));
+        scheduledTaskExecutor = new SimpleScheduledTaskExecutor(null, executorService);
+        scheduledTaskExecutor.start();
+        initialized = true;
+      }
+    } finally {
+      initializationLock.unlock();
     }
   }
 

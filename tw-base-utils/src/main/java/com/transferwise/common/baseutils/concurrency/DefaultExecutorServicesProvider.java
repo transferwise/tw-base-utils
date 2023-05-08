32321b526cc90fff53fcf7cb70546f2843df7b8b
Kristo Kuusküll <kristo.kuuskull@transferwise.com>
2023-05-08 15:55:47 +0300

    Added support for Spring Boot 3.0. (#35)
    * Added support for Spring Boot 3.0.

--- tw-base-utils/src/main/java/com/transferwise/common/baseutils/concurrency/DefaultExecutorServicesProvider.java
+++ tw-base-utils/src/main/java/com/transferwise/common/baseutils/concurrency/DefaultExecutorServicesProvider.java
@@ -2,27 +2,36 @@ package com.transferwise.common.baseutils.concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class DefaultExecutorServicesProvider implements IExecutorServicesProvider {
+public class DefaultExecutorServicesProvider implements IExecutorServicesProvider, InitializingBean, DisposableBean {
 
   private ExecutorService executorService;
   private ScheduledTaskExecutor scheduledTaskExecutor;
 
   private boolean initialized = false;
 
-  @PostConstruct
-  public synchronized void init() {
-    if (!initialized) {
-      executorService = Executors.newCachedThreadPool(new CountingThreadFactory("tw-base"));
-      scheduledTaskExecutor = new SimpleScheduledTaskExecutor(null, executorService);
-      scheduledTaskExecutor.start();
-      initialized = true;
+  private Lock initializationLock = new ReentrantLock();
+
+  @Override
+  public void afterPropertiesSet() {
+    initializationLock.lock();
+    try {
+      if (!initialized) {
+        executorService = Executors.newCachedThreadPool(new CountingThreadFactory("tw-base"));
+        scheduledTaskExecutor = new SimpleScheduledTaskExecutor(null, executorService);
+        scheduledTaskExecutor.start();
+        initialized = true;
+      }
+    } finally {
+      initializationLock.unlock();
     }
   }
 
-  @PreDestroy
+  @Override
   public void destroy() {
     scheduledTaskExecutor.stop();
     executorService.shutdownNow();

9b86ce37cd1e11191b55ddfa036ae6d06a89a81d
Jarvis <adam@jarvis.gg>
2023-08-16 12:46:57 -0500

    Migrate JsonConverter helper from commons (#40)

---
+++ tw-base-utils/src/main/java/com/transferwise/common/baseutils/jackson/JsonConverter.java
@@ -0,0 +1,19 @@
+package com.transferwise.common.baseutils.jackson;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.List;
+
+public interface JsonConverter {
+
+  <T> T toObject(String json, Class<T> clazz);
+
+  <T> T toObject(byte[] json, Class<T> clazz);
+
+  String fromObject(Object o);
+
+  byte[] fromObjectToBytes(Object o);
+
+  <T> List<T> toList(String json, TypeReference<List<T>> typeRef);
+
+  <T> List<T> toList(byte[] json, TypeReference<List<T>> typeRef);
+}

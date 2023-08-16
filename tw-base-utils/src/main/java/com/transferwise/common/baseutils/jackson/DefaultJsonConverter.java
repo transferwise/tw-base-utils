a037d8a45d4fd8bb15bd441053a44b6009724ac0
Jarvis <adam.jarvis@transferwise.com>
2023-08-16 11:35:40 -0500

    Migrate JsonConverter helper from commons

---
+++ tw-base-utils/src/main/java/com/transferwise/common/baseutils/jackson/DefaultJsonConverter.java
@@ -0,0 +1,57 @@
+package com.transferwise.common.baseutils.jackson;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.transferwise.common.baseutils.ExceptionUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+final class DefaultJsonConverter implements JsonConverter {
+
+  private final ObjectMapper objectMapper;
+
+  public DefaultJsonConverter(ObjectMapper injectedObjectMapper) {
+    this.objectMapper = injectedObjectMapper;
+    objectMapper.registerModule(JavaTimeModuleFactory.consistentMillisecondsTimeModule());
+    objectMapper.registerModule(new Jdk8Module());
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> T toObject(String json, Class<T> clazz) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.readerFor(clazz).readValue(json));
+  }
+
+  @Override
+  public <T> T toObject(byte[] json, Class<T> clazz) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.readValue(json, clazz));
+  }
+
+  @Override
+  public String fromObject(Object o) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.writer().writeValueAsString(o));
+  }
+
+  @Override
+  public byte[] fromObjectToBytes(Object o) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.writeValueAsBytes(o));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> List<T> toList(String json, TypeReference<List<T>> typeRef) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.readValue(json, typeRef));
+  }
+
+  @Override
+  public <T> List<T> toList(byte[] json, TypeReference<List<T>> typeRef) {
+    return ExceptionUtils.doUnchecked(() -> objectMapper.readValue(json, typeRef));
+  }
+}
